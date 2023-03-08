fb6b7c6e0fb7782faedc227c4d6b47377a1f8a26
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-03-08 19:03:40 +0100

    finish the page

--- src/components/Work.js
+++ src/components/Work.js
@@ -15,7 +15,7 @@ const Work = () => {
     <section className='section' id='work'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
-          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+          <motion.div variants={fadeIn('right', 0.2)} initial="hidden" whileInView={"show"} viewport={{once: false, amount: 0.3}} className='flex-1 flex flex-col gap-y-10 mb-10 lg:mb-0'>
             {/* Text */}
             <div>
               <h2 className='h2 leading-tight text-accent'>My Lastest <br />Work</h2>
@@ -23,37 +23,7 @@ const Work = () => {
               <button className='btn btn-sm'>View all projects</button>
             </div>
             {/* Imagen */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
-              {/* Overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              {/* Img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt=' '></img>
-              {/* pretite */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient'>UX/UI Desing</span>
-              </div>
-              {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3x1 text-white'>Project Tittle</span>
-              </div>
-            </div>
-            <div className='flex-1 flex flex-col gap-y-10'>
-            {/* imagne */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
-              {/* Overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              {/* Img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt=' '></img>
-              {/* pretite */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient'>UX/UI Desing</span>
-              </div>
-              {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3x1 text-white'>Project Tittle</span>
-              </div>
-            </div>
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               {/* Overlay */}
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               {/* Img */}
@@ -68,11 +38,42 @@ const Work = () => {
               </div>
             </div>
 
-          </div>
-          </div>
-          
+          </motion.div>
+          <motion.div variants={fadeIn('left', 0.2)} initial="hidden" whileInView={"show"} viewport={{once: false, amount: 0.3}} className='flex-1 flex flex-col gap-y-16'>
+            {/* imagne */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* Overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* Img */}
+              <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt=' '></img>
+              {/* pretite */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>UX/UI Desing</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3x1 text-white'>Project Tittle</span>
+              </div>
+            </div>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* Overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* Img */}
+              <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt=' '></img>
+              {/* pretite */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>UX/UI Desing</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3x1 text-white'>Project Tittle</span>
+              </div>
+            </div>
+
+          </motion.div>
+
         </div>
-        
+
       </div>
     </section>
   )
